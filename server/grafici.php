9285fe78426270d68340e208f6e9c806da76c059
Matteo Gheza <matteo.gheza07@gmail.com>
2020-05-26 10:01:54 +0200

    Grafici

--- server/grafici.php
+++ server/grafici.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html>
- <head>
-  <title>Sito in Manutenzione</title>
-  <meta charset="UTF-8">
-  <style>
-   h1 { font-size: 50px; }
-   body { text-align:center; font: 20px Helvetica, sans-serif; color: #333; }
-  </style>
- </head>
- <body>
-  <h1>Pagina in Manutenzione</h1>
-  <p>Scusaci per l'incoveniente. Stiamo lavorando per migliorare il servizio.</p>
-  <p>Saremo di nuovo attivi entro breve. Puoi utilizzare il resto del sito</p>
-  <p style="margin-top: 40px; font-size: 14px; color: #666"><em>il Webmaster</em></p>
- </body>
-</html>
+<?php
+require_once 'ui.php';
+loadtemplate('grafici.html', ['titolo' => 'Grafici']);
