41071dba0084c1658fdd9a519427f0128f3ca985
Matteo Gheza <matteo.gheza07@gmail.com>
2020-07-03 12:43:37 +0200

    User form translations

---
+++ server/translations/en/edit_user.php
@@ -0,0 +1,18 @@
+<?php
+// Translated by: Name Surname (github, email or another social profile url/username)
+
+return [
+    "E-mail" => "E-mail",
+    "user@provider-email-domain.com" => "user@provider-email-domain.com",
+    "Name" => "Name",
+    "Username" => "Username",
+    "Password" => "Password",
+    "Birthday" => "Birthday",
+    "Foreman" => "Foreman",
+    "Driver" => "Driver",
+    "Visible" => "Visible",
+    "Enabled" => "Enabled",
+    "Submit" => "Submit",
+    "Are you sure you want to delete the service?" => "Are you sure you want to delete the service?",
+    "The action cannot be canceled. Are you sure you want to continue?" => "The action cannot be canceled. Are you sure you want to continue?"
+];
