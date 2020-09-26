425c30f5af8b8c927cb03444a8a94801c478b96f
Matteo Gheza <matteo.gheza07@gmail.com>
2020-09-26 16:58:02 +0200

    csfr token in List

--- server/list.php
+++ server/list.php
@@ -1,4 +1,7 @@
 <?php
 require_once 'ui.php';
-loadtemplate('list.html', ['title' => t("Availability List",false)]);
+if(!isset($_GET["_tracy_bar"])){
+    $_SESSION["token_list"] = bin2hex(random_bytes(64));
+}
+loadtemplate('list.html', ['title' => t("Availability List",false), 'token_list' => $_SESSION['token_list']]);
 bdump($_SESSION);
--- server/risorse/ajax/ajax_aggiornadispo.php
+++ server/risorse/ajax/ajax_aggiornadispo.php
@@ -2,10 +2,10 @@
 include_once '../../core.php';
 init_class();
 $user->requirelogin();
-if(isset($_POST["change_id"]) && $_POST["dispo"] == 1){
+if(isset($_POST["change_id"]) && $_POST["dispo"] == 1 && $_POST["token_list"] == $_SESSION['token_list']){
      $risultato = $database->exec("UPDATE `%PREFIX%_profiles` SET `available` = '1' WHERE `%PREFIX%_profiles`.`id` = :id;", false, [":id" => $_POST["change_id"]]);
      $user->log("Attivazione disponibilita'", $_POST["change_id"], $user->auth->getUserId(), date("d/m/Y"), date("H:i.s"));
-} else if(isset($_POST["change_id"]) && $_POST["dispo"] == 0){
+} else if(isset($_POST["change_id"]) && $_POST["dispo"] == 0 && $_POST["token_list"] == $_SESSION['token_list']){
      $risultato = $database->exec("UPDATE `%PREFIX%_profiles` SET `available` = '0' WHERE `%PREFIX%_profiles`.`id` = :id;", false, [":id" => $_POST["change_id"]]);
      $user->log("Rimozione disponibilita'", $_POST["change_id"], $user->auth->getUserId(), date("d/m/Y"), date("H:i.s"));
 }
