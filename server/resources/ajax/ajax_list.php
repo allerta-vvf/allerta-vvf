be939d859ea5854ea2387c294420e67125f88e76
Matteo Gheza <matteo.gheza07@gmail.com>
2020-10-27 00:18:25 +0100

    [skip-ci] Initial accessibility improvements

--- server/resources/ajax/ajax_list.php
+++ server/resources/ajax/ajax_list.php
@@ -34,12 +34,12 @@ foreach($risultato as $row){
         $name = $user->nameById($row["id"]);
         $name_encoded = urlencode($name);
         $functionName = $row["available"] ? "Deactivate" : "Activate";
-        $firstCell = $row["chief"] ? "<a onclick='$functionName(".$row["id"].");'><img src='./resources/images/red_helmet.png' width='20px'>$name</a>" : "<a onclick='$callFunction(".$row["id"].");'><img src='./resources/images/black_helmet.png' width='20px'>$name</a>";
+        $firstCell = $row["chief"] ? "<a onclick='$functionName(".$row["id"].");'><img alt="chief" src='./resources/images/red_helmet.png' width='20px'>$name</a>" : "<a onclick='$callFunction(".$row["id"].");'><img alt="normal user" src='./resources/images/black_helmet.png' width='20px'>$name</a>";
         $secondCell = $row["available"] ? "<a onclick='$functionName(".$row["id"].");'><i class='fa fa-check' style='color:green'></i></a>" : "<a onclick='$callFunction(".$row["id"].");'><i class='fa fa-times'  style='color:red'></i></a>";
         $response[] = [
           (time()-$row["online_time"])<=30 ? "<u>".$firstCell."</u>" : $firstCell,
           $secondCell,
-          $row['autista'] ? "<img src='./resources/images/wheel.png' width='20px'>" : "",
+          $row['autista'] ? "<img alt="driver" src='./resources/images/wheel.png' width='20px'>" : "",
           $row['telefono'] ? "<a href='tel:+".$row['telefono']."'><i class='fa fa-phone'></i></a>" : "",
           $row['telefono'] ? "<a href='https://api.whatsapp.com/send?phone=+".$row['telefono']."text=ALLERTA IN CORSO.%20Mettiti%20in%20contatto%20con%20$name_encoded'><i class='fa fa-whatsapp' style='color:green'></i></a>" : "",
           $row['services'],
