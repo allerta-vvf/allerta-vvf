c2057e5c6ea64f964af0bb051d8c627c3303ab68
Matteo Gheza <matteo.gheza07@gmail.com>
2021-01-03 00:39:03 +0100

    Added option in installer and htaccess rule

--- server/install/installHelper.php
+++ server/install/installHelper.php
@@ -489,7 +489,8 @@ function initOptions($name, $visible, $developer, $password, $report_email, $own
             'training_edit' => 1,
             'training_remove' => 1,
             'use_location_picker' => 1,
-            'force_language' => 0
+            'force_language' => 0,
+            'force_remember_cookie' => 0
         ];
         $query = "";
         foreach ($options as $key => $value) {
