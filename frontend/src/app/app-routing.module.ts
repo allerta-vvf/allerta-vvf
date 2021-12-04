59f7fe27d1200ebbeecb74f6dccbe059d458a41e
Matteo Gheza <matteo.gheza07@gmail.com>
2021-12-04 21:36:11 +0100

    Add Angular

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ListComponent } from './_components/list/list.component';
+import { LogsComponent } from './_components/logs/logs.component';
+import { ServicesComponent } from './_components/services/services.component';
+import { TrainingsComponent } from './_components/trainings/trainings.component';
+
+const routes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: 'logs', component: LogsComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'trainings', component: TrainingsComponent },
+  //
+  { path: "**", redirectTo: "/list", pathMatch: "full" },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { ListComponent } from './_components/list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
