0ca94d48acf0eedfa986f6a880ea94d5e92e0863
Matteo Gheza <matteo.gheza07@gmail.com>
2022-01-10 23:59:44 +0100

    Add route loading spinner

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from './_services/auth.service';
 import { versions } from 'src/environments/versions';
+import { Router, RouteConfigLoadStart, RouteConfigLoadEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,19 @@ export class AppComponent {
   public menuButtonClicked = false;
   public revision_datetime_string;
   public versions = versions;
+  public loadingRoute = false;
 
-  constructor(public auth: AuthService) {
+  constructor(public auth: AuthService, private router: Router) {
     this.revision_datetime_string = new Date(versions.revision_timestamp).toLocaleString(undefined,  { day: '2-digit', month: '2-digit', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' });
+  }
+
+  ngOnInit () {
+    this.router.events.subscribe((event) => {
+      if (event instanceof RouteConfigLoadStart) {
+        this.loadingRoute = true;
+      } else if (event instanceof RouteConfigLoadEnd) {
+        this.loadingRoute = false;
+      }
+    });
   }
 }
