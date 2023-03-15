07e308c37423a3147ba71de34d0af0d41985e29f
Matteo Gheza <matteo.gheza07@gmail.com>
2023-03-15 23:11:36 +0100

    Enable manual mode when user changes availability

--- backend/app/Http/Controllers/AvailabilityController.php
+++ backend/app/Http/Controllers/AvailabilityController.php
@@ -24,6 +24,7 @@ class AvailabilityController extends Controller
         }
 
         $user->available = $request->input("available", false);
+        $user->availability_manual_mode = true;
         $user->save();
 
         return [
