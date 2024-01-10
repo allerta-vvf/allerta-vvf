14b8cd7d73ee2a562f32f60e544c77b87be964a9
Matteo Gheza <matteo.gheza07@gmail.com>
2024-01-10 01:03:09 +0100

    Remove file upload functionality in admin-info component
    The user no longer wants it... 10 minutes after requesting

--- frontend/src/app/_routes/admin/info/admin-info.component.ts
+++ frontend/src/app/_routes/admin/info/admin-info.component.ts
@@ -9,11 +9,6 @@ import Swal from 'sweetalert2';
   styleUrls: ['./admin-info.component.scss']
 })
 export class AdminInfoComponent implements OnInit {
-  allowedImageTypes = ["application/pdf"];
-  maxImageSize = 1024 * 1024 * 50; //50MB
-
-  LIFMNotUploadedYet = false;
-
   constructor(
     private translateService: TranslateService,
     private api: ApiClientService
@@ -21,55 +16,4 @@ export class AdminInfoComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  onLIFMSelected(event: any) {
-    const file: File = event.target.files[0];
-  
-    if (file) {
-      if(!this.allowedImageTypes.includes(file.type)) {
-        event.target.value = null;
-        Swal.fire({
-          title: this.translateService.instant("error_title"),
-          text: this.translateService.instant("validation.document_format_not_supported"),
-          icon: 'error',
-          confirmButtonText: 'Ok'
-        });
-        return;
-      }
-      if(file.size > this.maxImageSize) {
-        event.target.value = null;
-        Swal.fire({
-          title: this.translateService.instant("error_title"),
-          text: this.translateService.instant("validation.file_too_big"),
-          icon: 'error',
-          confirmButtonText: 'Ok'
-        });
-        return;
-      }
-      this.LIFMNotUploadedYet = true;
-    }
-  }
-
-  uploadLIFM(input: HTMLInputElement) {
-    if(!input.files || !input.files[0]) return;
-
-    console.log(input.files[0]);
-
-    const formData = new FormData();
-    formData.append('file', input.files[0], input.files[0].name);
-
-    this.api.post("admin/lifm", formData).then((response: any) => {
-      console.log(response);
-      this.LIFMNotUploadedYet = false;
-    }).catch((err: any) => {
-      console.log(err);
-      Swal.fire({
-        title: this.translateService.instant("error_title"),
-        text: err.error.message,
-        icon: 'error',
-        confirmButtonText: 'Ok'
-      });
-    });
-  }
-
 }
