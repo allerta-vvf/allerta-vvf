3d2c6c123e404f88a0f17f39a5417f73341b609c
Matteo <matteo.gheza07@gmail.com>
2025-05-22 23:43:35 +0200

    Start UI migration to Blade templates
    Using some cheap hosting providers, a standalone UI with Angular and connecting it to a Laravel as an api server can be very difficult to maintain and introduces strange bugs (especially with authentication).
    If I move everything server-side, the UX for both me and end users can improve a lot (faster loading times, thanks to server-side caching and no need for multiple API calls to backend to check authentication).

---
+++ backend/lang/it/list.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'set_available' => 'Imposta disponibile',
+    'set_unavailable' => 'Imposta non disponibile',
+    'enable_schedules' => 'Abilita programmazione oraria',
+    'disable_schedules' => 'Disabilita programmazione oraria',
+    'update_schedules' => 'Aggiorna orari',
+    'set_success' => 'è stata impostata con successo.',
+    'removed_success' => 'è stata rimossa con successo.',
+    'availability_change_failed' => 'Impossibile cambiare la disponibilità.',
+    'manual_mode_updated_successfully' => 'Modalità manuale aggiornata con successo.',
+    'manual_mode_update_failed' => 'Impossibile aggiornare la modalità manuale.'
+];
